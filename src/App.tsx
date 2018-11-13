a1584757b339525052a98c99f876768f6f22d8db
Eddie Chen <agameofprivacy@gmail.com>
2018-11-13 15:43:20 +0800

    typo

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ class App extends Component<Props> {
                 <a className="nav-link" href="#">開團資訊 <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="http://equallove.tw">關於婚影平權大平台</a>
+                <a className="nav-link" href="http://equallove.tw">關於婚姻平權大平台</a>
               </li>
             </ul>
           </div>
