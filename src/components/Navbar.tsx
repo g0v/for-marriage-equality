dbf20b7e3896e40f8284c221c84173d23f0bc570
Eddie Chen <agameofprivacy@gmail.com>
2018-11-13 21:03:31 +0800

    mobile somewhat

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default function Navbar(props: any) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">兩好三壞，全台開團資訊（11/12更新）</a>
+            <a className="navbar-brand" href="#">兩好三壞，全台開團資訊</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
