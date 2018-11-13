d7f7e562586cf7c6138670fd493a2547b72c1221
Kevin Liao <me@liaokev.in>
2018-11-13 19:09:43 +0800

    Fix bug in selector.

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -10,13 +10,12 @@ export interface Props {
 export default class Selector extends Component<Props> {
     render() {
         return (
-            <select className="custom-select custom-select-lg mb-3" value={this.props.title}>
-                <option value="" onClick={this.props.onChange}>{this.props.defaultTitle}</option>
+            <select onChange={this.props.onChange} value={this.props.title} className="custom-select custom-select-lg mb-3">
+                <option>{this.props.defaultTitle}</option>
                 {this.props.options.map((option, index) => {
                     return <option 
                         className="dropdown-item" 
                         key={index+1}
-                        onClick={this.props.onChange}
                     >{option}</option>
                 })}
             </select>
