29656e27a8b02373cd82deeb20982ff32dc334b0
Eddie Chen <agameofprivacy@gmail.com>
2018-11-13 19:10:07 +0800

    added title attr to buttons

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -9,25 +9,28 @@ class Action extends Component<Props> {
     render() {
         const { type, contact } = this.props
         var cn = "btn"
-        var link;
-        var buttonText;
+        var link
+        var buttonText
+        var title = ""
+
         switch(type) {
             case "line":
                 cn += " btn--line"
                 link = `line://ti/p/@${contact}`
-                // buttonText = `Line @${contact}`
+                title = `${contact}`
                 buttonText = `Line`
                 break;
             case "phone":
                 cn += " btn--phone"
                 buttonText = `打電話`
+                title = `${contact}`
                 link = `tel://${contact}`
                 break;
             default:
             break;
         }
         return (
-            <a className={cn} href={link}>
+            <a className={cn} href={link} title={title}>
                 {buttonText}
             </a>
         )
