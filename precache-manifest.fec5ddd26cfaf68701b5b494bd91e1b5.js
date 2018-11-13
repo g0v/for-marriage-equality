1ecfd62727f768a43b2232a7706fd89085ecc8fc
Kevin Liao <me@liaokev.in>
2018-11-13 14:19:28 +0800

    Updates

---
+++ precache-manifest.fec5ddd26cfaf68701b5b494bd91e1b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2037202b4c2c15dbfc",
+    "url": "/for-marriage-equality/static/js/runtime~main.4d203720.js"
+  },
+  {
+    "revision": "91983332dddf29b59b44",
+    "url": "/for-marriage-equality/static/js/main.91983332.chunk.js"
+  },
+  {
+    "revision": "e52be0586825a4e9d78d",
+    "url": "/for-marriage-equality/static/js/1.e52be058.chunk.js"
+  },
+  {
+    "revision": "91983332dddf29b59b44",
+    "url": "/for-marriage-equality/static/css/main.66864dd4.chunk.css"
+  },
+  {
+    "revision": "e52be0586825a4e9d78d",
+    "url": "/for-marriage-equality/static/css/1.13a1d46d.chunk.css"
+  },
+  {
+    "revision": "b164389de63b26b14bd171eb14002c9e",
+    "url": "/for-marriage-equality/index.html"
+  }
+];
