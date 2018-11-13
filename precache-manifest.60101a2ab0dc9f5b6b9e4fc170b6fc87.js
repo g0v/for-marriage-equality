60a4afda17acafe39c98cb6f41e427263ee16c05
Kevin Liao <me@liaokev.in>
2018-11-13 22:35:21 +0800

    Updates

--- precache-manifest.60101a2ab0dc9f5b6b9e4fc170b6fc87.js
+++ precache-manifest.60101a2ab0dc9f5b6b9e4fc170b6fc87.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/for-marriage-equality/static/js/runtime~main.4d203720.js"
   },
   {
-    "revision": "d4bde44ce027ae0f2f9f",
-    "url": "/for-marriage-equality/static/js/main.d4bde44c.chunk.js"
+    "revision": "f53a6004e07bf9ad7ebd",
+    "url": "/for-marriage-equality/static/js/main.f53a6004.chunk.js"
   },
   {
     "revision": "5114577a458abf5cdd1f",
     "url": "/for-marriage-equality/static/js/1.5114577a.chunk.js"
   },
   {
-    "revision": "d4bde44ce027ae0f2f9f",
+    "revision": "f53a6004e07bf9ad7ebd",
     "url": "/for-marriage-equality/static/css/main.3a1d7bb9.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/for-marriage-equality/static/css/1.b1f205e5.chunk.css"
   },
   {
-    "revision": "3637dc44ef6bd7bc0c3fc428ab78731c",
+    "revision": "155c62bcad77de6836d0d314e3d1e472",
     "url": "/for-marriage-equality/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/for-marriage-equality/precache-manifest.005500ba46492027f550f637b616bc30.js"
+  "/for-marriage-equality/precache-manifest.60101a2ab0dc9f5b6b9e4fc170b6fc87.js"
 );
 
 workbox.clientsClaim();
