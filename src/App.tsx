163df31bade3d9288dab4a73c5d5b30fe000231b
Eddie Chen <agameofprivacy@gmail.com>
2018-11-13 14:01:55 +0800

    took out nav and updated some styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,6 @@ class App extends Component<Props> {
 
     return (
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
         <Gallery shifts={shifts} />
       </div>
       );
