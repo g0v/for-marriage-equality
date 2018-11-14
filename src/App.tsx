dfffa7ee8294fd6c239b8fcdc9fa7beef6fe9e86
Kevin Liao <me@liaokev.in>
2018-11-14 21:09:30 +0800

    Fix.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Gallery from "./components/Gallery";
 import Canvass, { Area, getAreas } from "./canvass";
 import Navbar from "./components/Navbar";
 import Selector from "./components/Selector";
--import G0vbar from "./components/g0vbar";                                                                          │
+import G0vbar from "./components/g0vbar";
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
