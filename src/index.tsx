d73267eb256de328082c36bab68b89375b2045eb
Kevin Liao <me@liaokev.in>
2018-11-19 14:06:36 +0800

    Change Spreadsheet ID to correct one.

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,10 @@ import Canvass from './canvass';
 import * as serviceWorker from './serviceWorker';
 import GetSheetDone from 'get-sheet-done';
 
-const googleSheet: string = "131ImXHRXARx8j8t9esNCJhrLUfZQG347L1k3GsJ1m1Q";
+const googleSheet: string = "1i3Y4VTelUNFySPXMys6rLlN3wZlC9T_vLKR1X-9rnmY";
+const sheetNum: number = 2;
 
-GetSheetDone.labeledCols(googleSheet).then((sheet:any) => {
+GetSheetDone.labeledCols(googleSheet, sheetNum).then((sheet:any) => {
     console.log("Got: ", sheet);
     const rawData = sheet.data;
     const shifts: Array<Canvass> = rawData.map((a: any) => { return new Canvass(a) });
