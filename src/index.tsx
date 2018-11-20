0d00c655544e4cd96f308e8331a239b373661df6
Kevin Liao <me@liaokev.in>
2018-11-20 14:47:19 +0800

    Lint index.

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,24 @@
+// tslint:disable-next-line:import-name
+import GetSheetDone from 'get-sheet-done';
+// tslint:disable-next-line:import-name
 import React, { Component } from 'react';
+// tslint:disable-next-line:import-name
 import ReactDOM from 'react-dom';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GetSheetDone from 'get-sheet-done';
-
 import 'react-datepicker/dist/react-datepicker.css';
-
 import './index.scss';
-
-// import G0vbar from 'g0v-banner';
 
 import App from './App';
 import Canvass from './Canvass';
 
-const googleSheet: string = "1i3Y4VTelUNFySPXMys6rLlN3wZlC9T_vLKR1X-9rnmY";
+const googleSheet: string = '1i3Y4VTelUNFySPXMys6rLlN3wZlC9T_vLKR1X-9rnmY';
 const sheetNum: number = 2;
 
-GetSheetDone.labeledCols(googleSheet, sheetNum).then((sheet:any) => {
-    const rawData = sheet.data;
-    const shifts: Canvass[]= rawData.map((a: any) => new Canvass(a));
+GetSheetDone.labeledCols(googleSheet, sheetNum).then((sheet: any) => {
+  const rawData = sheet.data;
+  const shifts: Canvass[] = rawData.map((a: any) => new Canvass(a));
 
-    ReactDOM.render(<App shifts={shifts} />, document.getElementById('root'));
+  ReactDOM.render(<App shifts={shifts} />, document.getElementById('root'));
 });
