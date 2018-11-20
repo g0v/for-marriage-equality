549515e23c56d4a6310730adfe109807f00bd158
Kevin Liao <me@liaokev.in>
2018-11-20 15:04:32 +0800

    Lint Label.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,40 +1,41 @@
+// tslint:disable-next-line:import-name
 import React, { Component } from 'react';
 
-export interface Props {
-    text: string
+export interface IProps {
+  text: string;
 }
 
-class Label extends Component<Props> {
-    render() {
-        const { text } = this.props
-        var cn = "label"
-        switch(text) {
-            case "發文宣":
-                cn += " label--neutral"
-                break;
-            case "對話":
-                cn += " label--prominent"
-                break;
-            case "代工":
-                cn += " label--muted"
-                break;
-            case "掃街":
-                cn += " label--chill"
-                break;
-            case "擺攤":
-                cn += " label--warm"
-            break;
+class Label extends Component<IProps> {
+  public render() {
+    const { text } = this.props;
+    let cn = 'label';
+    switch (text) {
+      case '發文宣':
+        cn += ' label--neutral';
+        break;
+      case '對話':
+        cn += ' label--prominent';
+        break;
+      case '代工':
+        cn += ' label--muted';
+        break;
+      case '掃街':
+        cn += ' label--chill';
+        break;
+      case '擺攤':
+        cn += ' label--warm';
+        break;
 
-            default:
-                cn += " label--distance"
-            break;
-        }
-        return (
-            <div className={cn}>
-                {text}
-            </div>
-        )
+      default:
+        cn += ' label--distance';
+        break;
     }
+    return (
+      <div className={cn}>
+        {text}
+      </div>
+    );
+  }
 }
 
-export default Label
+export default Label;
