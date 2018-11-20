64e6c62b492623d540210fed6cef6963419646d2
Kevin Liao <me@liaokev.in>
2018-11-20 15:10:27 +0800

    Lint KeyValue and NavBar.

--- src/components/KeyValue.tsx
+++ src/components/KeyValue.tsx
@@ -1,20 +1,21 @@
+// tslint:disable-next-line:import-name
 import React, { Component } from 'react';
 
-export interface Props {
-    k: string;
-    v: string;
+export interface IProps {
+  k: string;
+  v: string;
 }
 
-class KeyValue extends Component<Props> {
-    render() {
-        const { k, v } = this.props
-        return (
-            <div className="stat">
-                <label className="stat__label">{k}</label>
-                <span className="stat__figure">{v}</span>
-            </div>
-        )
-    }
+class KeyValue extends Component<IProps> {
+  public render() {
+    const { k, v } = this.props;
+    return (
+      <div className="stat">
+        <label className="stat__label">{k}</label>
+        <span className="stat__figure">{v}</span>
+      </div>
+    );
+  }
 }
 
-export default KeyValue
+export default KeyValue;
