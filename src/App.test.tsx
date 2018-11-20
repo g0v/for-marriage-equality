ac470bc141fbc9be71517a3d6fcb95485ab5ec73
Kevin Liao <me@liaokev.in>
2018-11-20 15:08:19 +0800

    Gut Tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,3 @@ import React from 'react';
 // tslint:disable-next-line:import-name
 import ReactDOM from 'react-dom';
 import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
